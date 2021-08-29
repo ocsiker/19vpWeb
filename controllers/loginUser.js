caff03bd801abafaab76bc704b5e2601bddcf4f0
Ocsiker <huynhqthang89@gmail.com>
2021-08-30 03:55:51 +0700

    add content-1 content-2

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,6 @@
-const bcrypt = require('bcrypt');
-const User   = require('../models/listUser');
+const bcrypt     = require('bcrypt');
+const User       = require('../models/listUser');
+const ListCourse = require('../models/listCourse');
 
 module.exports = (req, res) => {
     const {username, password} = req.body;
@@ -10,8 +11,12 @@ module.exports = (req, res) => {
             bcrypt.compare(password, user.password, (err, same) => {
                 if (same)
                 {
+                    const listcourses = ListCourse.find({});
+
                     req.session.userId = user._id;
-                    res.redirect('/');
+                    res.render(
+                        'index',
+                        {"user" : "user", "listcourses" : "listcourses"});
                 }
                 else
                     res.redirect('/');
